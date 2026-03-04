b690e37eefd16d3c83625edec992aba82e65fb5d
shafrinakterr <shafrinakterr@gmail.com>
2026-03-04 11:49:33 +0600

    footer menu added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,7 @@
+import twiter from "../../assets/socail-icon/Group.png";
+import linkedin from "../../assets/socail-icon/Group1.png";
+import facebook from "../../assets/socail-icon/Group2.png";
+import email from "../../assets/socail-icon/fi_6244710.png";
 const Footer = () => {
   return (
     <div className=" bg-black text-base-content md:p-10 p-3 ">
@@ -13,30 +17,41 @@ const Footer = () => {
           </p>
         </aside>
         <nav>
-          <h6 className="text-xl text-white">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
           <h6 className="text-xl text-white">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">About Us</a>
+          <a className="link link-hover">Our Mission</a>
+          <a className="link link-hover">Contact Saled</a>
+        </nav>
+        <nav>
+          <h6 className="text-xl text-white">Services</h6>
+          <a className="link link-hover">Products & Services</a>
+          <a className="link link-hover">Customer Stories</a>
+          <a className="link link-hover">Download Apps</a>
+        </nav>
+        <nav>
+          <h6 className="text-xl text-white">Information</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Join Us</a>
         </nav>
         <nav>
           <h6 className="text-xl text-white">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-        <nav>
-          <h6 className="text-xl text-white">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <div className="flex items-center gap-2">
+            <img src={twiter} />
+            <a className="link link-hover">@CS — Ticket System</a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src={linkedin} /> @CS — Ticket System
+            <a className="link link-hover"></a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src={facebook} /> @CS — Ticket System
+            <a className="link link-hover"></a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src={email} /> support@cst.com
+            <a className="link link-hover flex items-center"></a>
+          </div>
         </nav>
       </footer>
       <p className="text-center py-3 text-[#A1A1AA]  ">
